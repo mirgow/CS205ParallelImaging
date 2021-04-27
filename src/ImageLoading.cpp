1dfacc495aace22f12d4ab32192ead69611e2008
Ubuntu <ubuntu@ip-172-31-88-231.ec2.internal>
2021-04-27 15:07:49 +0000

    reorganize scripts

--- src/ImageLoading.cpp
+++ src/ImageLoading.cpp
@@ -12,9 +12,6 @@
 using namespace cv;
 using namespace std;
 
-
-
-
 int main (int argc, char* argv[])
 {
 
--- src/sequential.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/tracking.hpp>
-using namespace std;
-int main()
-{
-    cout << "hello world";
-    return 0;
-}
